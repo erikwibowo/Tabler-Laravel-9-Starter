2af01c86eb486fd24943f9d30b09cc56766d17aa
erikwibowo <erikwibo@gmail.com>
2022-03-19 02:10:41 +0700

    add : install laravel ui (bootstrap)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::middleware('role:admin')->get('/dashboard', function(){
+    return 'Dashboard';
+})->name('dashboard');
