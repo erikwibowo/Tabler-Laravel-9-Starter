fe5664ed0bcb29a509f78c893c02889b896c6539
erikwibowo <erikwibo@gmail.com>
2022-03-19 13:06:42 +0700

    add : setting up dashboard and user page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -28,7 +29,15 @@ Auth::routes([
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('admin')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('admin');
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('user', 'index')->middleware(['permission:index user'])->name('user.index');
+        Route::post('user', 'store')->middleware(['permission:store user'])->name('user.store');
+        Route::get('user/{id}', 'show')->middleware(['permission:show user'])->name('user.show');
+        Route::put('user/{id}', 'update')->middleware(['permission:update user'])->name('user.update');
+        Route::delete('user/{id}', 'delete')->middleware(['permission:delete user'])->name('user.delete');
+    });
 });
